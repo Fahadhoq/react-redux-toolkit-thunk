a1d2572cdfe04652a448cf75c457331009b95bd1
FahadulHoq <fahadulhoq@gmail.com>
2024-09-13 23:48:00 +0600

    redux looger axios

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import { type } from '@testing-library/user-event/dist/type';
+import React from 'react';
+
+const Home = () => {
+   
+
+    return (
+        <p>ss</p>
+    );
+};
+
+export default Home;
---
+++ src/service/store/counterStore.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+import counterReducer from "../reducers/counterReducer";
+
+const counterStore = createStore(counterReducer);
+export default counterStore;
